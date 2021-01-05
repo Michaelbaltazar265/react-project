7cc6ff65b5b5e2bdb71abc113fc315e7f6ff4b06
Michaelbaltaza265 <michaelbaltazar265@gmail.com>
2021-01-05 03:35:24 +0000

    adding a coming soon and text

--- 87lux/src/ComingSoon.jsx
+++ 87lux/src/ComingSoon.jsx
@@ -1,39 +1,21 @@
 import React from "react";
-import FlipNumbers from "react-flip-numbers";
 
-class ComingSoon extends React.Component {
-    state = {
-      timeRemaining: 10000
-    };
-  
-    componentDidMount() {
-      this.timer = setInterval(() => {
-        this.setState({
-          timeRemaining: this.state.timeRemaining - 1
-        });
-      }, 1000);
-    }
-  
-    componentWillUnmount() {
-      clearInterval(this.timer);
-    }
-  
-    render() {
-      return (
-        <div className="container">
-          <div className="App-intro">
-              <h1 className='text-center'> Comming Soon</h1>
-            <FlipNumbers
-              play
-              width={50}
-              height={50}
-              numbers={`${this.state.timeRemaining}`}
-            />
-            <h1 className='text-center'>Minutes</h1>
-          </div>
-        </div>
-      );
-    }
-  }
+const ComingSoon = () => {
+  return (
+    <div className="container">
+      <div className="App-intro">
+        <h1 className="text-center"> Coming Soon</h1>
+        <p className='text-center'>
+          {" "}
+          We are working hard to get this up and running! <br /> Go here to
+          check on our last news{" "}
+          <a href="https://www.youtube.com/channel/UC46wWUso9H5KPQcoL9iE3Ug">
+            youtube.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default ComingSoon;
