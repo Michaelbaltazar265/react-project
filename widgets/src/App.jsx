0604536cc06bace476a34fe86cd164c72820ed46
Michael <michaelbaltazar265@gmail.com>
2021-04-12 16:53:58 +0000

    translate-feature

--- widgets/src/App.jsx
+++ widgets/src/App.jsx
@@ -1,29 +1,7 @@
 import React from "react";
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from "./components/Dropdown";
+
 import Translate from "./components/Translate";
 
-
-
-// const options = [
-//   {
-//     label: "The Color Red",
-//     value: "red",
-//   },
-//   {
-//     label: "The Color Green",
-//     value: "green",
-//   },
-//   {
-//     label: "A Shade of Blue",
-//     value: "blue",
-//   },
-//   {
-//     label: "A Shade of Purple",
-//     value: "purple",
-//   },
-// ];
 
 const App = () => {
  
